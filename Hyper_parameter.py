ecc480b3872be752b5b578da11e5d13984fbeeb0
Kerem Dülger <124495822+bumkerembum@users.noreply.github.com>
2025-06-12 14:00:48 +0300

    Update Hyper_parameter.py

--- Hyper_parameter.py
+++ Hyper_parameter.py
@@ -8,36 +8,34 @@ import pandas as pd
 import optuna
 from sklearn.preprocessing import StandardScaler
 
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 tf.compat.v1.disable_eager_execution()
 tf.compat.v1.reset_default_graph()
 
 
 # Import Files
-x_tr_data = pd.read_excel('//home//kerem//Desktop//Airfoil//scenario4//training_x.xlsx', header = None)
+x_tr_data = pd.read_excel('location', header = None)
 X_Train = np.array(x_tr_data)
-                                                                                                                                        
-x_veri_data = pd.read_excel('//home//kerem//Desktop//Airfoil//scenario4//validation_x.xlsx', header = None)
+
+x_veri_data = pd.read_excel('location', header = None)
 X_Veri = np.array(x_veri_data)
 
-y_tr_data = pd.read_excel('//home//kerem//Desktop//Airfoil//scenario4//training_y.xlsx', header = None)
+y_tr_data = pd.read_excel('location', header = None)
 y_Train = np.array(y_tr_data)
 
-y_veri_data = pd.read_excel('//home//kerem//Desktop//Airfoil//scenario4//validation_y.xlsx', header = None)
+y_veri_data = pd.read_excel('location', header = None)
 y_Veri = np.array(y_veri_data)
 
-
-scaler_drag = StandardScaler()
 scaler_lift = StandardScaler()
 scaler_moment = StandardScaler()
 
 y_Train_scaled = np.column_stack((
-    scaler_drag.fit_transform(y_Train[:, 0].reshape(-1, 1)).ravel(),
     scaler_lift.fit_transform(y_Train[:, 1].reshape(-1, 1)).ravel(),
     scaler_moment.fit_transform(y_Train[:, 2].reshape(-1, 1)).ravel()
 ))
 
 y_Veri_scaled = np.column_stack((
-    scaler_drag.transform(y_Veri[:, 0].reshape(-1, 1)).ravel(),
     scaler_lift.transform(y_Veri[:, 1].reshape(-1, 1)).ravel(),
     scaler_moment.transform(y_Veri[:, 2].reshape(-1, 1)).ravel()
 ))
@@ -51,7 +49,7 @@ dim = X_Veri.shape[1]
 tstart = time.time()
 
 # Define the SN class
-class SobolevNetwork:
+class Network:
     def __init__(self, input_dim, hidden_dims, activation_funcs):
         self.input_dim = input_dim
         self.hidden_dims = hidden_dims
@@ -63,8 +61,8 @@ class SobolevNetwork:
             b = tf.Variable(tf.ones([dim]))
             self.weights.append((W, b))
             prev_dim = dim
-        self.W_out = tf.Variable(tf.random.normal([prev_dim, 3], stddev=0.1))
-        self.b_out = tf.Variable(tf.ones([3]))
+        self.W_out = tf.Variable(tf.random.normal([prev_dim, 2], stddev=0.1))
+        self.b_out = tf.Variable(tf.ones([2]))
 
     def forward(self, X):
         out = X
@@ -81,7 +79,7 @@ def objective(trial):
     batch_size = 700
     
     # Hyperparameters to tune
-    num_hidden_layers = trial.suggest_int('num_hidden_layers', low=1, high=5, step=1)
+    num_hidden_layers = trial.suggest_int('num_hidden_layers', low=3, high=5, step=1)
     hidden_units = trial.suggest_int('n_units', low=1, high=64, step=1)  # single value for all layers
     hidden_dims = [hidden_units] * num_hidden_layers
     learning_rate = trial.suggest_loguniform('learning_rate', 1e-4, 1e-1)
@@ -93,19 +91,20 @@ def objective(trial):
     }
     hidden_activations = [trial.suggest_categorical(f'activation_l{i}', list(activation_functions.values())) for i in range(num_hidden_layers)]
 
+
+
     # Define the network
-    model = SobolevNetwork(dim, hidden_dims, hidden_activations)
+    model = Network(dim, hidden_dims, hidden_activations)
     
     X = tf.compat.v1.placeholder(tf.float32, shape=[None, dim], name='X')
-    y = tf.compat.v1.placeholder(tf.float32, shape=[None, 3], name='y')
+    y = tf.compat.v1.placeholder(tf.float32, shape=[None, 2], name='y')
     
     y_p = model.forward(X)
 
     
-    loss_drag = tf.reduce_mean(tf.abs((y_p[:,0] - tf.reshape(y, [batch_size, 3])[:,0])/tf.reshape(y,[batch_size,3])[:,0]))
-    loss_lift = tf.reduce_mean(tf.abs((y_p[:,1] - tf.reshape(y, [batch_size, 3])[:,1])/tf.reshape(y,[batch_size,3])[:,1]))
-    loss_moment = tf.reduce_mean(tf.abs((y_p[:,2] - tf.reshape(y, [batch_size, 3])[:,2])/tf.reshape(y,[batch_size,3])[:,2]))
-    loss = (loss_drag + loss_lift + loss_moment) / 3
+    loss_lift = tf.reduce_mean(tf.abs((y_p[:,0] - tf.reshape(y, [batch_size, 2])[:,0])/tf.reshape(y,[batch_size,2])[:,0]))
+    loss_moment = tf.reduce_mean(tf.abs((y_p[:,1] - tf.reshape(y, [batch_size, 2])[:,1])/tf.reshape(y,[batch_size,2])[:,1]))
+    loss = (loss_lift + loss_moment ) / 2
     
     optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
     train_op = optimizer.minimize(loss)
@@ -124,12 +123,12 @@ def objective(trial):
     y_valid_pred = sess.run(y_p, feed_dict={X: X_Veri})
     
     
-    validation_loss_drag = np.mean(np.abs((y_valid_pred[:,0] - y_Veri_scaled[:,0])/y_Veri_scaled[:,0]))
-    validation_loss_lift = np.mean(np.abs((y_valid_pred[:,1] - y_Veri_scaled[:,1])/y_Veri_scaled[:,1]))
-    validation_loss_moment = np.mean(np.abs((y_valid_pred[:,2] - y_Veri_scaled[:,2])/y_Veri_scaled[:,2]))
+    validation_loss_lift = np.mean(np.abs((y_valid_pred[:,0] - y_Veri_scaled[:,0])/y_Veri_scaled[:,0]))
+    validation_loss_moment = np.mean(np.abs((y_valid_pred[:,1] - y_Veri_scaled[:,1])/y_Veri_scaled[:,1]))
+
   
-    validation_loss = (validation_loss_drag + validation_loss_lift + validation_loss_moment) /3
-     
+    validation_loss = (validation_loss_lift + validation_loss_moment) / 2
+                                                
     
     # Updata best loss
     
@@ -145,38 +144,38 @@ def objective(trial):
     # 15  7  3 = 25 benim method için ok değil ama optimizasyon için ok 
   
     if validation_loss <= objective.best_val_loss:
-       objective.best_val_loss_drag = validation_loss_drag
        objective.best_val_loss_lift = validation_loss_lift
        objective.best_val_loss_moment = validation_loss_moment
+
        objective.best_val_loss = validation_loss
     
   
     
     print("\n")
-    print(f"Trial {trial.number} - Loss Drag: {validation_loss_drag:.7f}, Loss Lift: {validation_loss_lift:.7f}, Loss Moment: {validation_loss_moment:.7f}")
+    print(f"Trial {trial.number} - Loss Lift: {validation_loss_lift:.7f}, Loss Moment: {validation_loss_moment:.7f}")
 
     # Calculate criterion
-    re = np.linalg.norm(y_valid_pred - y_Veri) / np.linalg.norm(y_Veri) * 100
-    print(f"Trial {trial.number} - Criterion: {re:.4f}")
+    #re = np.linalg.norm(y_valid_pred - y_Veri) / np.linalg.norm(y_Veri) * 100
+    #print(f"Trial {trial.number} - Criterion: {re:.4f}")
     
-    if re < objective.best_re:
-        objective.best_re = re
-        objective.best_trial = trial.number
+    #if re < objective.best_re:
+        #objective.best_re = re
+        #objective.best_trial = trial.number
 
-    print(f"Best criterion so far: {objective.best_re:.4f} in trial {objective.best_trial}")
-    print("\n")
-    print(f"Best Validation Losses: Drag={objective.best_val_loss_drag:.7f}, Lift={objective.best_val_loss_lift:.7f}, Moment={objective.best_val_loss_moment:.7f}")
+    #print(f"Best criterion so far: {objective.best_re:.4f} in trial {objective.best_trial}")
+    #print("\n")
+    print(f"Best Validation Losses: Lift={objective.best_val_loss_lift:.7f}, Moment={objective.best_val_loss_moment:.7f}")
     print(f"Best Total Loss: {objective.best_val_loss}")
     print("\n\n")
 
     return validation_loss
 
 objective.best_val_loss = float("inf")
-objective.best_val_loss_drag = float('inf')
 objective.best_val_loss_lift = float('inf')
 objective.best_val_loss_moment = float('inf')
 objective.best_re = 1000   # initial value
 objective.best_trial = 0   # initial value
+
 
 # Run the hyperparameter optimization
 study = optuna.create_study(direction='minimize')
